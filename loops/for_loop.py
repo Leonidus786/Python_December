a824fb7aef31606a1faf28547edbb6b1591fc40d
Leonidus786 <abhi.upadh@gmail.com>
2024-01-11 22:01:45 +0530

    Loops In Python

---
+++ loops/for_loop.py
@@ -0,0 +1,28 @@
+# print("Hello\n")
+# print("Hello\n")
+# print("Hello\n")
+# print("Hello\n")
+
+
+'''
+Loops ---> Iterative statement --> Repetitive statement 
+
+repeat hone wali chiz
+
+---Jo v item Sequuence yani lagatar rkhi jane wali chizein
+
+list()-->[]
+
+tuple()-->()
+
+dict()-->{"key":value}
+
+set()-->{}
+
+'''
+
+fruits = ["apple", "banana", "cherry", 14, 11.7, 3+5.6j, True, False, 'Amit', {1,2,3}, {"Name":'Alisha'}, (2.9,7,True,29,1+9J)]
+for x in fruits:
+  print(x)
+
+
