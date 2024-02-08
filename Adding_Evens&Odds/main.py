afcfe073306f9bc4b65e8fd65127417fbfc56c9c
Leonidus786 <abhi.upadh@gmail.com>
2024-02-08 22:03:49 +0530

    For loops---> Project - Password Generator, Adding Even & Odds, Highest Score, Avreage Height, and Fizz Buzz

---
+++ Adding_Evens&Odds/main.py
@@ -0,0 +1,61 @@
+#Write your code below this row 👇
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# even_sum = 0
+# for number in range(2, 101, 2):
+#   print(number)
+#   even_sum += number
+# print(even_sum)
+  
+#or
+
+# alternative_sum = 0
+# for number in range(1, 101):
+#   if number % 2 == 0:
+#     # print(number)
+#     alternative_sum += number
+# print(alternative_sum)
+
+
+
+
+alternative_sum = 0
+
+for number in range(0,100):
+    if number % 2 != 0:
+        # print(number)
+        alternative_sum += number
+print(alternative_sum)
+
+
+
+
+
+
+
