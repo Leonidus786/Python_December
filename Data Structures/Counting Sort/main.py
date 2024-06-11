65388be4a8fa260c4300f55cf5e7e7ea672b0747
Leonidus786 <abhi.upadh@gmail.com>
2024-06-11 21:58:44 +0530

    Coffee_Machine, Counting Sort

---
+++ Data Structures/Counting Sort/main.py
@@ -0,0 +1,18 @@
+def countingSort(arr):
+    max_val = max(arr)
+    count = [0] * (max_val + 1)
+
+    while len(arr) > 0:
+        num = arr.pop(0)
+        count[num] += 1
+
+    for i in range(len(count)):
+        while count[i] > 0:
+            arr.append(i)
+            count[i] -= 1
+
+    return arr
+
+unsortedArr = [4, 2, 2, 6, 3, 3, 1, 6, 5, 2, 3]
+sortedArr = countingSort(unsortedArr)
+print("Sorted array:", sortedArr)
